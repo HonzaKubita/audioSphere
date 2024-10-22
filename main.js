587e726699d54efbebb2fdfe9ceb6eef5796a103
Honza Kubita <honza.kubita@gmail.com>
2024-10-22 18:13:09 +0200

    feat: add fullscreen

--- main.js
+++ main.js
@@ -36,3 +36,20 @@ document.getElementById("file").addEventListener("change", (e) => {
   const file = e.target.files[0];
   init(file);
 });
+
+let fullscreen = false;
+
+const fullscreenButton = document.getElementById("fullscreen-button");
+
+// Fullscreen button
+fullscreenButton.addEventListener("click", () => {
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullscreenButton.src = "icons/fullscreen_enter.svg";
+    fullscreen = false;
+  } else {
+    document.documentElement.requestFullscreen();
+    fullscreenButton.src = "icons/fullscreen_exit.svg";
+    fullscreen = true;
+  }
+});
